2a770f6e3292576f52aa3b7a67f5bef2f3f73745
Nandini Yadav <86313679+NandiniMnnit@users.noreply.github.com>
2021-10-16 02:29:51 +0530

    Add files via upload
    database connetion

---
+++ dbconnect.php
@@ -0,0 +1,14 @@
+<?php
+   $servername="localhost";
+   $username="root";
+   $password="";
+   $database="medical";
+
+    //database connection
+   $conn=mysqli_connect("$servername","$username","$password","$database");
+  
+  
+ 
+
+   
+?>
